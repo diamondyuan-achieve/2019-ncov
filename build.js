0530d9da7d5929ebdc6aaa25f9e846a99ae13357
DiamondYuan <541832074@qq.com>
2020-01-21 21:37:59 +0800

    :building_construction: add data hash when build

--- build.js
+++ build.js
@@ -1,9 +1,19 @@
 const fs = require("fs");
 const path = require("path");
+const md5 = require("md5");
 
 const dist = path.resolve(__dirname, "dist");
 
-const time = Date.now();
+const dataSource = path.resolve(__dirname, "src/data");
+const files = fs
+  .readdirSync(dataSource)
+  .map(o => path.resolve(dataSource, o))
+  .map(file => fs.readFileSync(file, { encoding: "utf-8" }))
+  .join("\n");
+
+const hash = md5(files);
+
+console.log(`data hash is ${hash}`);
 
 [
   "components.css.map",
@@ -13,7 +23,7 @@ const time = Date.now();
   "main.css"
 ].forEach(file => {
   if (fs.existsSync(path.join(dist, file))) {
-    fs.renameSync(path.join(dist, file), path.join(dist, `${time}-${file}`));
+    fs.renameSync(path.join(dist, file), path.join(dist, `${hash}-${file}`));
   }
 });
 
@@ -22,7 +32,7 @@ let indexHtml = fs.readFileSync(path.join(dist, "index.html"), {
 });
 
 ["components.css", "main.js", "main.css"].forEach(file => {
-  indexHtml = indexHtml.replace(file, `${time}-${file}`);
+  indexHtml = indexHtml.replace(file, `${hash}-${file}`);
 });
 
 fs.writeFileSync(path.join(dist, "index.html"), indexHtml);
